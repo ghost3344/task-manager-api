0cca1be1934d0918a5273832063d3548d0e314d0
kashyap a jivani <kashyapjivani@gmail.com>
2021-07-20 14:00:21 +0530

    database correction

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectionURL = process.env.MONGO_DB_URL
 const databaseName = 'task-manager-api'
-mongoose.connect(connectionURL+'/'+databaseName,{useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true})
+mongoose.connect(connectionURL,{useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true})
 
 
 
